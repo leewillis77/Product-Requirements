5e238e37b34557185b30541fc43e92a73fafc332
Lee Willis <leewillis77@gmail.com>
2014-04-21 10:08:56 +0100

    Remove unneeded widget setting

--- templates/pblw-requirements-widget-settings.php
+++ templates/pblw-requirements-widget-settings.php
@@ -1,11 +1,5 @@
 <!-- Title -->
 <p>
-	<label for="<?php esc_attr_e( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'pblw_docs' ); ?></label>
+	<label for="<?php esc_attr_e( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'pblw_reqs' ); ?></label>
 	<input class="widefat" id="<?php esc_attr_e( $this->get_field_id( 'title' ) ); ?>" name="<?php esc_attr_e( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php esc_attr_e( $title ); ?>" />
-</p>
-
-<!-- Count -->
-<p>
-	<label for="<?php esc_attr_e( $this->get_field_id( 'count' ) ); ?>"><?php _e( 'Number of items:', 'pblw_docs' ); ?></label>
-	<input class="widefat" id="<?php esc_attr_e( $this->get_field_id( 'count' ) ); ?>" name="<?php esc_attr_e( $this->get_field_name( 'count' ) ); ?>" type="text" value="<?php esc_attr_e( $count ); ?>" />
 </p>
